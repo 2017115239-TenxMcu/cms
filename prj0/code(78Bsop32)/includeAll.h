9d56958f02ef28bc423e424ad195af8d8ffc6383
2017115239-TenxMcu <1770274322@qq.com>
2019-11-29 21:38:40 +0800

    vscode代码格式化，h文件按字母排序导致错误

--- prj0/code(78Bsop32)/includeAll.h
+++ prj0/code(78Bsop32)/includeAll.h
@@ -1,6 +1,11 @@
-#include <REGtenxTM52F5278B.h>
+ #include <REGtenxTM52F5278B.h>
+ #include <INTRINS.h>
+
+ #include "typeAlias.h"
 
  #include "display.h"
  #include "init.h"
+ #include "keys.h"
  #include "main.h"
  #include "typeAlias.h"
+ #include <INTRINS.H> 
