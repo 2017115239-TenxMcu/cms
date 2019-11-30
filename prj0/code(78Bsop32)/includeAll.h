651cb1ee545a82df7890bcf32c7ac3173015fba0
2017115239-TenxMcu <1770274322@qq.com>
2019-11-30 14:36:05 +0800

    led灯点亮的实验接线图

--- prj0/code(78Bsop32)/includeAll.h
+++ prj0/code(78Bsop32)/includeAll.h
@@ -1,11 +1,6 @@
  #include <REGtenxTM52F5278B.h>
- #include <INTRINS.h>
-
  #include "typeAlias.h"
-
  #include "display.h"
  #include "init.h"
- #include "keys.h"
  #include "main.h"
- #include "typeAlias.h"
  #include <INTRINS.H> 
