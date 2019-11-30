a1cd8e8579a9397a23e800169b2b8d285ecda5bd
2017115239-TenxMcu <1770274322@qq.com>
2019-11-30 14:36:05 +0800

    led灯的点亮实验代码

--- prj0/code(78Bsop32)/main.c
+++ prj0/code(78Bsop32)/main.c
@@ -1,7 +1,8 @@
 #define _main_c
 #include "includeAll.h"
-
+//---------------------------------------------
 void main(){
+   //初始化
    SysInit(); 
    VarsInit();
    while(1){
@@ -11,15 +12,13 @@ void main(){
       delayMs(500);
       F_ledOn();
       delayMs(500);
-
    }
 }
- //
+//---------------------------------------------
 void delayMs(uint16_t msCount){
     uint16_t i ,j;
-    for(uint16_t i = 0; i< msCount; i++){
-        for(uint16_t j = 0; j<1000; j++){
-
+    for(i = 0; i< msCount; i++){
+        for(j = 0; j<1000; j++){
         }
     }
 
